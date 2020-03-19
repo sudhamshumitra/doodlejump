a5b97a7a8aa8fc64191c106a3528fa6062f80d78
Sudhamshu Mitra <sudhamshu.mitra@gmail.com>
2020-03-19 23:56:45 +0530

    added tilt feature

--- doodleJump.cs
+++ doodleJump.cs
@@ -23,15 +23,22 @@ void Start()
       
     }
 
+    private void FixedUpdate()
+    {
+        //movement based on mobile tilt
+        Vector3 tilt = Input.acceleration;
+        transform.Translate(Input.acceleration.x * 6 * Time.deltaTime, 
+                             0, 0);
+       
+    }
+   
     // Update is called once per frame
     void Update()
     {
         Vector3 a = Input.acceleration;
         Vector3 position = transform.position;
         Vector3 velocity = rb.velocity;
-        //Move based on mobile tilt
-        //velocity.x = rb.velocity.x * 2; 
-        rb.AddForce(new Vector2 (a.x, 0), ForceMode2D.Impulse);
+        
 
         //Ensure that when the character is going up, there is no collision
         if (rb.velocity.y >= 0){
@@ -42,12 +49,16 @@ void Start()
         }
 
         //For debugging easily on laptop, have to remove this
-        if (Input.GetKeyDown(KeyCode.RightArrow)){
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             rb.AddForce(Vector2.right * 2, ForceMode2D.Impulse);
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             rb.AddForce(Vector2.left * 2, ForceMode2D.Impulse);
         }
-       
+
+
         //To ensure character comes back to screen when moved out of screen
         if (position.x > stageDimensions.x){
             position.x = -stageDimensions.x;
